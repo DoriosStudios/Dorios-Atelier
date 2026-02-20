5356cea42cd9efbb3be24244010f060bdfad5092
Kauziin_ <74920731+Kauziin@users.noreply.github.com>
2026-02-20 11:32:49 -0300

    refactor(data/scripts): modularize doriosapi bootstrap

---
+++ Data/scripts/doriosAPI/config.js
@@ -0,0 +1,13 @@
+export const addonData = {
+    name: "Dorios' Atelier",
+    author: "Dorios Studios",
+    identifier: "dorios_atelier",
+    version: "1.0.3"
+}
+
+import './API.js'
+import './dependencyChecker.js'
+import './blockClass.js'
+import './playerClass.js'
+import './itemStackClass.js'
+import './entityClass.js'
